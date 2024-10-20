d034828d7e7d95f8a5cd33ca597433e0bb6c048e
valenndy <andyv2048@gmail.com>
2024-10-20 16:48:58 -0500

    base sin errores y conexion con firebase

---
+++ MERCO242/app/src/main/java/com/example/merco_242/domain/model/Message.kt
@@ -0,0 +1,6 @@
+package com.example.merco_242.domain.model
+
+data class Message(
+    var id:String = "",
+    var content:String = ""
+)
--- README.md
+++
@@ -1,5 +0,0 @@
-# Merco-242
-
-- **Alexis Jaramillo (A00395655)**
-- **Andy Valencia (A00)**
-- **Fabiana Valderruten (A00)**
