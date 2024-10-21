81735bc39937eeb988638b8fb6a5ff026a85aab0
FFabianna <favaso1016@gmai.com>
2024-10-21 11:01:31 -0500

    Feat: Registro de usuario

---
+++ app/src/main/java/com/example/merco242/domain/model/Message.kt
@@ -0,0 +1,6 @@
+package com.example.merco242.domain.model
+
+data class Message(
+    var id:String = "",
+    var content:String = ""
+)
---
+++ app/src/main/java/com/example/merco242/repository/AuthRepository.kt
@@ -0,0 +1,44 @@
+package com.example.merco242.repository
+
+import android.util.Log
+import com.example.merco242.domain.model.User
+import com.example.merco242.service.AuthService
+import com.example.merco242.service.AuthServiceImpl
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
+
+interface AuthRepository {
+    suspend fun signup(user:User, password:String)
+    suspend fun signin(email:String, password:String)
+}
+
+class AuthRepositoryImpl(
+    val authService: AuthService = AuthServiceImpl(),
+    val userRepository: UserRepository = UserRepositoryImpl()
+) : AuthRepository{
+    override suspend fun signup(user: User, password: String) {
+        //1. Registro en modulo de autenticación
+        authService.createUser(user.email, password)
+        Log.e("AuthRepositoryImpl", "Usuario registrado con éxito")
+        //2. Obtenemos el UID
+        val uid = Firebase.auth.currentUser?.uid
+        Log.v("AuthRepositoryImpl", "UID: $uid")
+        //3. Crear el usuario en Firestore
+        uid?.let {
+            user.id = it
+            Log.e("AuthRepositoryImpl   USEDID", user.id)
+            userRepository.createUser(user)
+        }
+        Log.e("AuthRepositoryImpl", "Usuario registrado con éxito")
+    }
+
+    override suspend fun signin(email: String, password: String) {
+        authService.loginWithEmailAndPassword(email, password)
+    }
+}
+
